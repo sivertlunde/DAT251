5afa1b8b14fb1a0799b6ac366cdd5b700231a191
adrsolheim <adrsolheim2@gmail.com>
2021-03-15 14:38:20 +0100

    Add firestore setup. Ignore private keys

---
+++ backend/src/main/java/no/hvl/dat251/backend/firestore/FirestoreConfig.java
@@ -0,0 +1,15 @@
+package no.hvl.dat251.backend.firestore;
+
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FirestoreConfig {
+
+    @Bean
+    public Firestore getDb() {
+        return FirestoreClient.getFirestore();
+    }
+}
