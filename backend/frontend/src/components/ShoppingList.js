316d71d3635e565bca8ad092aba96ad868429ec0
Jostein <571542@stud.hvl.no>
2021-03-16 16:55:24 +0100

    Router init

---
+++ backend/frontend/src/components/ShoppingList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withRouter } from "react-router";
+
+class ShoppingList extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1>Handleliste kommer her</h1>
+            </div>
+        )
+    }
+}
+
+export default withRouter(ShoppingList)
