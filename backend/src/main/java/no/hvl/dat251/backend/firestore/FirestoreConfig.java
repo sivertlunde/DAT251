c8ff3c0d09ac92fe23706f8f29248e527375b814
sivertlunde <sivert.lunde@gmail.com>
2021-03-24 14:13:28 +0100

    Fixed some clunky mistakes

--- backend/src/main/java/no/hvl/dat251/backend/firestore/FirestoreConfig.java
+++ backend/src/main/java/no/hvl/dat251/backend/firestore/FirestoreConfig.java
@@ -5,11 +5,11 @@ import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class FirestoreConfig {
-//
-//    @Bean
-//    public Firestore getDb() {
-//        return FirestoreClient.getFirestore();
-//    }
-//}
+@Configuration
+public class FirestoreConfig {
+
+    @Bean
+    public Firestore getDb() {
+        return FirestoreClient.getFirestore();
+    }
+}
